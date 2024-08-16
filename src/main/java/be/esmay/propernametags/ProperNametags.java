4d6329d7334a913fce767929685c3b964b8f8815
Esmaybe <esmaybe@craftville.net>
2024-08-16 16:44:40 +0200

    Packetevents instead of protocollib

--- src/main/java/be/esmay/propernametags/ProperNametags.java
+++ src/main/java/be/esmay/propernametags/ProperNametags.java
@@ -1,46 +1,38 @@
 package be.esmay.propernametags;
 
 import be.esmay.propernametags.api.configuration.DefaultConfiguration;
+import be.esmay.propernametags.api.objects.ProperNameTag;
 import be.esmay.propernametags.commands.ProperNametagsCommand;
 import be.esmay.propernametags.common.listeners.PlayerJoinListener;
 import be.esmay.propernametags.common.listeners.PlayerSneakListener;
-import be.esmay.propernametags.api.objects.ProperNameTag;
 import be.esmay.propernametags.common.listeners.PlayerTeleportListener;
 import be.esmay.propernametags.common.tasks.MountNametagTask;
 import be.esmay.propernametags.common.tasks.UpdateNameTask;
 import be.esmay.propernametags.common.tasks.UpdateVisibilityTask;
 import be.esmay.propernametags.utils.ChatUtils;
 import be.esmay.propernametags.utils.task.SteppingTaskRegistry;
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
-import com.comphenix.protocol.wrappers.WrappedDataValue;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
+import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes;
+import com.github.retrooper.packetevents.protocol.world.Location;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerDestroyEntities;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSetPassengers;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSpawnEntity;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.collect.Lists;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
 import lombok.Getter;
 import lombok.Setter;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -50,12 +42,10 @@ public final class ProperNametags extends JavaPlugin {
     private static ProperNametags instance;
 
     @Getter
-    private ProtocolManager protocolManager;
-
-    @Getter
     private final Set<ProperNameTag> nameTags = new HashSet<>();
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private DefaultConfiguration defaultConfiguration;
 
     private final AtomicInteger lastEntityId = new AtomicInteger(Integer.MAX_VALUE);
@@ -70,7 +60,8 @@ public final class ProperNametags extends JavaPlugin {
             .expireAfterWrite(2, TimeUnit.SECONDS)
             .build();
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private boolean nameTagVisible = true;
 
     public ProperNametags() {
@@ -79,7 +70,9 @@ public final class ProperNametags extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        this.protocolManager = ProtocolLibrary.getProtocolManager();
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().load();
+
         this.defaultConfiguration = new DefaultConfiguration(this);
     }
 
@@ -98,6 +91,8 @@ public final class ProperNametags extends JavaPlugin {
         Bukkit.getScoreboardManager().getMainScoreboard().getTeams().forEach(team -> {
             team.setOption(org.bukkit.scoreboard.Team.Option.NAME_TAG_VISIBILITY, org.bukkit.scoreboard.Team.OptionStatus.NEVER);
         });
+
+        PacketEvents.getAPI().init();
     }
 
     public void sendNameTag(Player player, Player viewer, boolean isLogin) {
@@ -105,21 +100,26 @@ public final class ProperNametags extends JavaPlugin {
         ProperNameTag nameTag = new ProperNameTag(player.getUniqueId(), viewer.getUniqueId(), entityId);
         this.nameTags.add(nameTag);
 
-        PacketContainer spawnEntityPacket = new PacketContainer(PacketType.Play.Server.SPAWN_ENTITY);
+        WrapperPlayServerSpawnEntity spawnEntityPacket = new WrapperPlayServerSpawnEntity(
+                entityId,
+                UUID.randomUUID(),
+                EntityTypes.ARMOR_STAND,
+                new Location(
+                        player.getLocation().getX(),
+                        player.getLocation().getY() + 1.8,
+                        player.getLocation().getZ(),
+                        player.getLocation().getYaw(),
+                        player.getLocation().getPitch()
+                ),
+                player.getYaw(),
+                0,
+                null);
 
-        spawnEntityPacket.getIntegers().write(0, entityId);
-        spawnEntityPacket.getUUIDs().write(0, UUID.randomUUID());
-        spawnEntityPacket.getEntityTypeModifier().write(0, EntityType.ARMOR_STAND);
+        WrapperPlayServerEntityMetadata entityMetadataPacket = this.createMetaData(player, viewer, player.isSneaking(), entityId);
+        if (entityMetadataPacket == null) return;
 
-        spawnEntityPacket.getDoubles()
-                .write(0, player.getLocation().getX())
-                .write(1, player.getLocation().getY() + 1.8)
-                .write(2, player.getLocation().getZ());
-
-        PacketContainer sneakPacket = this.createMetaData(player, viewer, player.isSneaking(), entityId);
-
-        this.protocolManager.sendServerPacket(viewer, spawnEntityPacket);
-        this.sendMetaData(viewer, sneakPacket);
+        PacketEvents.getAPI().getPlayerManager().sendPacket(viewer, spawnEntityPacket);
+        this.sendMetaData(viewer, entityMetadataPacket);
 
         Bukkit.getScheduler().runTaskLater(this, () -> this.sendMountNameTag(nameTag), isLogin ? 7L : 2L);
     }
@@ -132,21 +132,15 @@ public final class ProperNametags extends JavaPlugin {
 
         int entityId = nameTag.getEntityId();
 
-        PacketContainer packetContainer = new PacketContainer(PacketType.Play.Server.MOUNT);
-        packetContainer.getIntegers().write(0, player.getEntityId());
-        packetContainer.getIntegerArrays().write(0, new int[]{entityId});
-
-        this.protocolManager.sendServerPacket(viewer, packetContainer);
+        WrapperPlayServerSetPassengers packet = new WrapperPlayServerSetPassengers(player.getEntityId(), new int[]{entityId});
+        PacketEvents.getAPI().getPlayerManager().sendPacket(viewer, packet);
     }
 
-    public PacketContainer createMetaData(ProperNameTag nameTag, boolean sneaking) {
+    public WrapperPlayServerEntityMetadata createMetaData(ProperNameTag nameTag, boolean sneaking) {
         return this.createMetaData(Bukkit.getPlayer(nameTag.getPlayer()), Bukkit.getPlayer(nameTag.getViewer()), sneaking, nameTag.getEntityId());
     }
 
-    public PacketContainer createNameUpdate(ProperNameTag nameTag) {
-        PacketContainer metadataPacket = new PacketContainer(PacketType.Play.Server.ENTITY_METADATA);
-        metadataPacket.getIntegers().write(0, nameTag.getEntityId());
-
+    public WrapperPlayServerEntityMetadata createNameUpdate(ProperNameTag nameTag) {
         Player player = Bukkit.getPlayer(nameTag.getPlayer());
         if (player == null) return null;
 
@@ -158,27 +152,24 @@ public final class ProperNametags extends JavaPlugin {
         }
 
         Component tagComponent = ChatUtils.format(tag);
-        String json = GsonComponentSerializer.gson().serialize(tagComponent);
-        WrappedChatComponent component = WrappedChatComponent.fromJson(json);
 
-        List<WrappedDataValue> values = Lists.newArrayList(
-                new WrappedDataValue(2, WrappedDataWatcher.Registry.getChatComponentSerializer(true), Optional.of(component.getHandle()))
+        List<EntityData> entityData = List.of(
+                new EntityData(2, EntityDataTypes.OPTIONAL_ADV_COMPONENT, Optional.of(tagComponent))
         );
 
-        metadataPacket.getDataValueCollectionModifier().write(0, values);
-        return metadataPacket;
+        return new WrapperPlayServerEntityMetadata(
+                nameTag.getEntityId(),
+                entityData
+        );
     }
 
-    public PacketContainer createMetaData(Player player, Player viewer, boolean sneaking, Integer entityId) {
+    public WrapperPlayServerEntityMetadata createMetaData(Player player, Player viewer, boolean sneaking, Integer entityId) {
         if (entityId == null) {
             ProperNameTag nameTag = this.nameTags.stream().filter(tag -> tag.getPlayer().equals(player.getUniqueId()) && tag.getViewer().equals(viewer.getUniqueId())).findFirst().orElse(null);
             if (nameTag == null) return null;
 
             entityId = nameTag.getEntityId();
         }
-
-        PacketContainer metadataPacket = new PacketContainer(PacketType.Play.Server.ENTITY_METADATA);
-        metadataPacket.getIntegers().write(0, entityId);
 
         String tag = this.defaultConfiguration.getPrefix() + this.getDefaultConfiguration().getName() + this.defaultConfiguration.getSuffix();
         tag = tag.replaceAll("%player%", player.getName());
@@ -188,37 +179,46 @@ public final class ProperNametags extends JavaPlugin {
         }
 
         Component tagComponent = ChatUtils.format(tag);
-        String json = GsonComponentSerializer.gson().serialize(tagComponent);
-        WrappedChatComponent component = WrappedChatComponent.fromJson(json);
 
-        List<WrappedDataValue> values = Lists.newArrayList(
-                new WrappedDataValue(0, WrappedDataWatcher.Registry.get(Byte.class), sneaking ? (byte) 0x02 | 0x20 : (byte) 32),
-                new WrappedDataValue(2, WrappedDataWatcher.Registry.getChatComponentSerializer(true), Optional.of(component.getHandle())),
-                new WrappedDataValue(3, WrappedDataWatcher.Registry.get(Boolean.class), true),
-                new WrappedDataValue(15, WrappedDataWatcher.Registry.get(Byte.class), (byte) 16)
+        List<EntityData> entityData = List.of(
+                new EntityData(0, EntityDataTypes.BYTE, sneaking ? (byte) 0x02 | 0x20 : (byte) 32),
+                new EntityData(2, EntityDataTypes.OPTIONAL_ADV_COMPONENT, Optional.of(tagComponent)),
+                new EntityData(3, EntityDataTypes.BOOLEAN, true),
+                new EntityData(15, EntityDataTypes.BYTE, (byte) 16)
         );
 
-        metadataPacket.getDataValueCollectionModifier().write(0, values);
-        return metadataPacket;
+        return new WrapperPlayServerEntityMetadata(entityId, entityData);
     }
 
     public void sendMetaData(Player player, Player viewer, boolean sneaking, Integer entityId) {
-        PacketContainer packetContainer = this.createMetaData(player, viewer, sneaking, entityId);
-        this.protocolManager.sendServerPacket(viewer, packetContainer);
+        WrapperPlayServerEntityMetadata packetContainer = this.createMetaData(player, viewer, sneaking, entityId);
+        if (packetContainer == null) return;
+
+        PacketEvents.getAPI().getPlayerManager().sendPacket(viewer, packetContainer);
     }
 
     public void sendMetaData(ProperNameTag nameTag, boolean sneaking) {
-        PacketContainer packetContainer = this.createMetaData(nameTag, sneaking);
-        this.protocolManager.sendServerPacket(Bukkit.getPlayer(nameTag.getViewer()), packetContainer);
+        WrapperPlayServerEntityMetadata packetContainer = this.createMetaData(nameTag, sneaking);
+        if (packetContainer == null) return;
+
+        Player player = Bukkit.getPlayer(nameTag.getViewer());
+        if (player == null) return;
+
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packetContainer);
     }
 
     public void sendNameUpdate(ProperNameTag nameTag) {
-        PacketContainer packetContainer = this.createNameUpdate(nameTag);
-        this.protocolManager.sendServerPacket(Bukkit.getPlayer(nameTag.getViewer()), packetContainer);
+        WrapperPlayServerEntityMetadata packetContainer = this.createNameUpdate(nameTag);
+        if (packetContainer == null) return;
+
+        Player player = Bukkit.getPlayer(nameTag.getViewer());
+        if (player == null) return;
+
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packetContainer);
     }
 
-    public void sendMetaData(Player viewer, PacketContainer packetContainer) {
-        this.protocolManager.sendServerPacket(viewer, packetContainer);
+    public void sendMetaData(Player viewer, @NotNull WrapperPlayServerEntityMetadata packetContainer) {
+        PacketEvents.getAPI().getPlayerManager().sendPacket(viewer, packetContainer);
     }
 
     public boolean hasNameTag(Player player, Player viewer) {
@@ -228,21 +228,18 @@ public final class ProperNametags extends JavaPlugin {
     public void removeNameTags(Player player) {
         List<ProperNameTag> entities = new ArrayList<>(this.nameTags.stream().filter(nameTag -> nameTag.getPlayer().equals(player.getUniqueId())).toList());
 
-        entities.forEach(entity -> {
-            PacketContainer destroyEntityPacket = new PacketContainer(PacketType.Play.Server.ENTITY_DESTROY);
-            destroyEntityPacket.getModifier().write(0, IntArrayList.of(entity.getEntityId()));
+        WrapperPlayServerDestroyEntities packet = new WrapperPlayServerDestroyEntities(entities.stream().mapToInt(ProperNameTag::getEntityId).toArray());
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packet);
 
-            this.protocolManager.sendServerPacket(player, destroyEntityPacket);
-            this.nameTags.remove(entity);
-        });
+        entities.forEach(this.nameTags::remove);
     }
 
     public void removeNameTag(ProperNameTag nameTag) {
-        PacketContainer destroyEntityPacket = new PacketContainer(PacketType.Play.Server.ENTITY_DESTROY);
-        destroyEntityPacket.getModifier().writeDefaults();
-        destroyEntityPacket.getModifier().write(0, IntArrayList.of(nameTag.getEntityId()));
+        WrapperPlayServerDestroyEntities packet = new WrapperPlayServerDestroyEntities(nameTag.getEntityId());
+        Player player = Bukkit.getPlayer(nameTag.getViewer());
+        if (player == null) return;
 
-        this.protocolManager.sendServerPacket(Bukkit.getPlayer(nameTag.getViewer()), destroyEntityPacket);
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packet);
         this.nameTags.remove(nameTag);
     }
 
@@ -250,16 +247,13 @@ public final class ProperNametags extends JavaPlugin {
         ProperNameTag nameTag = this.nameTags.stream().filter(tag -> tag.getPlayer().equals(player.getUniqueId()) && tag.getViewer().equals(viewer.getUniqueId())).findFirst().orElse(null);
         if (nameTag == null) return;
 
-        PacketContainer destroyEntityPacket = new PacketContainer(PacketType.Play.Server.ENTITY_DESTROY);
-        destroyEntityPacket.getModifier().writeDefaults();
-        destroyEntityPacket.getModifier().write(0, IntArrayList.of(nameTag.getEntityId()));
-
-        this.protocolManager.sendServerPacket(viewer, destroyEntityPacket);
+        WrapperPlayServerDestroyEntities packet = new WrapperPlayServerDestroyEntities(nameTag.getEntityId());
+        PacketEvents.getAPI().getPlayerManager().sendPacket(viewer, packet);
         this.nameTags.remove(nameTag);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        PacketEvents.getAPI().terminate();
     }
 }
